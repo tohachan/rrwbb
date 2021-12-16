c254cfbf389934382534ec5a9df20e8b4fd3eec3
Anton An <toha.chan@gmail.com>
2021-12-16 15:37:27 +0700

    add GTM

--- src/server/renderer.js
+++ src/server/renderer.js
@@ -147,8 +147,21 @@ export default ({ clientStats, hot }) => (req, res, next) => {
                         
                           gtag('config', 'UA-215576548-1');
                         </script>
+
+                        <!-- Google Tag Manager -->
+                        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-KF6H98T');</script>
+                        <!-- End Google Tag Manager -->
                     </head>
                     <body>
+                        <!-- Google Tag Manager (noscript) -->
+                        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KF6H98T"
+                        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+                        <!-- End Google Tag Manager (noscript) -->
+
                         <section class="hero is-fullheight" id="app">${component}</section>
 
                         <script>window.__INITIAL__DATA__ = ${JSON.stringify(initialState)}</script>
